4039a23237f24723461423429e2c07e8047c4530
VictorMatheus819 <victormatheus819@gmail.com>
2021-09-07 18:09:35 -0300

    testes de controller estado e deficiencia

--- src/test/java/com/uff/sem_barreiras/controller/DeficienciaControllerTest.java
+++ src/test/java/com/uff/sem_barreiras/controller/DeficienciaControllerTest.java
@@ -2,7 +2,7 @@ package com.uff.sem_barreiras.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +10,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uff.sem_barreiras.model.Deficiencia;
 import com.uff.sem_barreiras.service.DeficienciaService;
 
 
@@ -19,6 +21,10 @@ class DeficienciaControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired
+    private ObjectMapper objectMapper;
+
+	
 	@MockBean
     private DeficienciaService   deficienciaService;
 
@@ -26,9 +32,27 @@ class DeficienciaControllerTest {
     public void verificarEndpointRecuperacaoDeficienciaPorId() throws Exception {
         this.mockMvc.perform(get("/deficiencia/1")).andExpect(MockMvcResultMatchers.status().isOk());
     }
+    
     @Test
-    public void verificarEndpointRecuperacaoEscolaridadeLista() throws Exception {
+    public void verificarEndpointRecuperacaoDeficienciaLista() throws Exception {
         this.mockMvc.perform(get("/deficiencia")).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    
+    @Test
+    public void verificarEndpointCriacaoDeficiencia() throws Exception {
+    	String  deficiencia = "baixa visao";
+    	
+    	Deficiencia def =  new Deficiencia();
+    	
+    	def.setDescricao(deficiencia);
+    	def.setNecessidadePisoTatil(true);
+    	def.setNecessidadeRampa(false);
+    	def.setNecessidadeSonora(true);
+		
+    	this.mockMvc.perform(post("/deficiencia")
+    	        .contentType("application/json")
+    	        .content(objectMapper.writeValueAsString(def)))
+    	        .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
